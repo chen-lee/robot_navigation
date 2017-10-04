1e10d6c04f7fc35ce61c7a2850273e7d5240435a
Your Name <you@example.com>
2017-10-04 17:50:26 +0800

    updata the get_coordinate

--- src/navigation_experiment/get_coordinate/src/get_coordinate.cpp
+++ src/navigation_experiment/get_coordinate/src/get_coordinate.cpp
@@ -1,136 +1,169 @@
-#include <string>
-#include <ros/ros.h>                           // 包含ROS的头文件
-#include <sensor_msgs/JointState.h>
-#include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
-#include <boost/asio.hpp>                  //包含boost库函数
-#include <boost/bind.hpp>
+#include <ros/ros.h>                      
 #include <math.h>
-#include "std_msgs/String.h"              //ros定义的String数据类型
-#include <stdio.h> /* Standard input/output definitions */
-#include <string.h> /* String function definitions */
-#include <unistd.h> /* UNIX standard function definitions */
-#include <fcntl.h> /* File control definitions */
-#include <errno.h> /* Error number definitions */
-#include <termios.h> /* POSIX terminal control definitions */
+#include <stdio.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <fcntl.h> 
+#include <assert.h> 
+#include <termios.h> 
 #include "get_coordinate/position.h"
+using namespace std;
 
-int distanceBaseA = 0;	
-int distanceBaseB = 0;
-int distanceBaseC = 0;
-int BaseAx =4370,BaseAy = 0;//int BaseAx = 0,BaseAy = 790 + 6 * 930 + 520; 6120
-int BaseBx = 0,BaseBy =  0;//int BaseBx = 600 + 6 * 870 + 50,BaseBy =  790 + 6 * 930 + 520;
-int BaseCx = 0,BaseCy = 5590;//7420;//int BaseCx = 600 + 3 * 870,BaseCy = 0;
-
-/*
- * @brief Open serial port with the given device name
- *
- * @return The file descriptor on success or -1 on error.
- */
-
-int open_port(char *port_device)
+class get_coordinate_two_dimension
 {
-    int fd; /* File descriptor for the port */
-
-    fd = open(port_device, O_RDWR | O_NOCTTY | O_NDELAY);
-    if (fd == -1)
-    {
-        perror("open_port: Unable to open /dev/ttyS0 - ");
-    }
-    else
-        fcntl(fd, F_SETFL, 0);
-
-    return (fd);
-}
-
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "position");
+public:
+    get_coordinate_two_dimension(char *port_device,int BaseAx,int BaseAy,
+                                 int BaseBx,int BaseBy,int BaseCx,int BaseCy, int rate);
+    void get_raw_data();
+    void print_info();
+    void parser_distance();
+    void parser_position();
+    void thread_work();
+    ~get_coordinate_two_dimension();
+   
+private:
+    int distanceBaseA;	
+    int distanceBaseB;
+    int distanceBaseC;
+    int BaseAx, BaseAy;
+    int BaseBx, BaseBy;
+    int BaseCx, BaseCy;
+    int x,y;
+    int fd;
+    int raw_data;
+    char buffer[1024];
+    char* buf;
+    int rate;
+    int rate_cnt;
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<get_coordinate::position>("position",1);
+    ros::Publisher pub;
+};
+
+get_coordinate_two_dimension::get_coordinate_two_dimension(char *port_device,int BaseAx,int BaseAy,
+                                                            int BaseBx,int BaseBy,int BaseCx,int BaseCy,int rate):
+                                                            BaseAx(BaseAx),BaseAy(BaseAy),BaseBx(BaseBx),
+                                                            BaseBy(BaseBy),BaseCx(BaseCx),BaseCy(BaseCy),rate(rate)
+{
+    cout << "construct funtion"  << endl;
+    buf = buffer;
+    //open serial port
+    fd = open(port_device, O_RDWR | O_NOCTTY | O_NDELAY);
+    cout << "fd:" << fd  << endl;
+	assert(fd != -1); 
+    fcntl(fd, F_SETFL, 0);
+    //serial initial
     struct termios options;
-
-    int fd=open_port("/dev/ttyUSB0");
-    if(fd==-1){
-        return -1;
-    }
-
     tcgetattr(fd, &options);
-
-
-    //Set the baud rates to 38400...
     cfsetispeed(&options, B115200);
     cfsetospeed(&options, B115200);
-
-
-    //Enable the receiver and set local mode...
     options.c_cflag |= (CLOCAL | CREAD);
-    options.c_cflag &= ~CSIZE; /* Mask the character size bits */
-    options.c_cflag |= CS8; /* Select 8 data bits */
-
-    //No parity
+    options.c_cflag &= ~CSIZE; 
+    options.c_cflag |= CS8; 
     options.c_cflag &= ~PARENB;
     options.c_cflag &= ~CSTOPB;
     options.c_cflag &= ~CSIZE;
     options.c_cflag |= CS8;
-
-
-    //Set the new options for the port...
     tcsetattr(fd, TCSANOW, &options);
+    //ros initial
+    //ros::init();
+    pub = n.advertise<get_coordinate::position>("position",1);
+}
 
-    int ax,ay,az,gx,gy,gz;
-    int cricleCnt = 0;
-    char buf[1024];
-    char* pos=buf;
+void get_coordinate_two_dimension::get_raw_data()
+{   
+    while(1)
+    {
+	    size_t n = read( fd, buf, 1);
+        if( n == 1)  
+        {
+		    if( *buf == '\n')
+			{
+			    *(buf + 1) = 0;
+				sscanf( buffer, "%d", &raw_data);
+				buf = buffer;
+                break;
+			}
+			else
+			{
+			    buf++;
+			}
+		}
+   }
+}
 
-    while(ros::ok()){
-        ssize_t n=read(fd, pos, 1);
-        if(n==1){
-            if(*pos=='\n' ){
-                *(pos+1)=0;
-                sscanf(buf,"%d,%d,%d,%d,%d,%d", &ax, &ay, &az, &gx, &gy, &gz);
-                if (ax%10 == 1)
-                {
-                  distanceBaseA = (ax - 1) / 10;
-                }
-                 if (ax%10 == 2)
-                {
-                  distanceBaseB = (ax - 2) / 10;
-                }
-                if (ax%10 == 3)
-                {
-                  distanceBaseC = (ax - 3) / 10;
-                }
-                if(cricleCnt++ ==5)
-                {
-                    
-                    cricleCnt = 0;
-                    int A = BaseAx - BaseCx;
-                    int B = BaseAy - BaseCy;
-                    float C = pow(BaseAx,2) - pow(BaseCx,2) + pow(BaseAy,2) - pow(BaseCy,2) + pow(distanceBaseC,2) - pow(distanceBaseA,2);
-                    int D = BaseBx - BaseCx;
-                    int E = BaseBy - BaseCy;
-                    float F = pow(BaseBx,2) - pow(BaseCx,2) + pow(BaseBy,2) - pow(BaseCy,2) + pow(distanceBaseC,2) - pow(distanceBaseB,2);
+void get_coordinate_two_dimension::print_info()
+{
+    printf("DISTANCE: %d %d %d  position:%d %d\n", 
+                     distanceBaseA,distanceBaseB,
+                     distanceBaseC,x,y);
+}
 
-                    int x = (B * F - E * C) / (2 * B *D - 2 * A * E);
-                    int y = (A * F - D * C) / (2 * A * E - 2 * B * D);
-                    get_coordinate::position msg;
-                    msg.positionX = x;
-                    msg.positionY = y;
-                    pub.publish(msg);
-                    printf("DISTANCE: %d %d %d %d %d\n", distanceBaseA,distanceBaseB,distanceBaseC,x,y);
-
-                }
-                //printf("DISTANCE: %d %d %d\n", distanceBaseA,distanceBaseB,distanceBaseC);
-                //printf("DISTANCE: %d\n", ax);
-
-                pos=buf;
-            }else{
-                pos++;
-            }
-        }
+void get_coordinate_two_dimension::parser_distance()
+{
+    if(raw_data % 10 == 1)
+    {
+        distanceBaseA = (raw_data - 1) / 10;
     }
+    if(raw_data % 10 == 2)
+    {
+        distanceBaseB = (raw_data - 2) / 10;
+    }
+    if(raw_data % 10 == 3)
+    {
+        distanceBaseC = (raw_data - 3) / 10;
+    }
+}
 
+void get_coordinate_two_dimension::parser_position()
+{
+    if(rate_cnt++ ==rate)
+    {
+        rate_cnt = 0;
+        int A = BaseAx - BaseCx;
+        int B = BaseAy - BaseCy;
+        float C = pow(BaseAx,2) - pow(BaseCx,2) + pow(BaseAy,2) 
+                  - pow(BaseCy,2) + pow(distanceBaseC,2) 
+                  - pow(distanceBaseA,2);
+        int D = BaseBx - BaseCx;
+        int E = BaseBy - BaseCy;
+        float F = pow(BaseBx,2) - pow(BaseCx,2) + pow(BaseBy,2) 
+                  - pow(BaseCy,2) + pow(distanceBaseC,2) 
+                  - pow(distanceBaseB,2);
+        x = (B * F - E * C) / (2 * B *D - 2 * A * E);
+        y = (A * F - D * C) / (2 * A * E - 2 * B * D);
+        
+        get_coordinate::position msg;
+        msg.positionX = x;
+        msg.positionY = y;
+        pub.publish(msg);
 
+        print_info();
+    }
+}
+
+void get_coordinate_two_dimension::thread_work()
+{
+    while( ros::ok() )
+    {
+        get_raw_data();
+        parser_distance();
+        parser_position();
+    }
+}
+
+get_coordinate_two_dimension::~get_coordinate_two_dimension()
+{
     close(fd);
+}
+ 
+int main(int argc, char **argv)
+{
+    //ros initial
+    ros::init(argc, argv, "position");
+    ros::NodeHandle n;
+    ros::Publisher pub = n.advertise<get_coordinate::position>("position",1);
+    get_coordinate_two_dimension *p = new get_coordinate_two_dimension( 
+                                     "/dev/ttyUSB0", 4370, 0, 0, 0, 0, 5590,5);
+    p->thread_work();
+    delete p;
 }
